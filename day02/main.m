9dec9c3573a708a96430b1145ab4cb3e47240bd0
赵凌云 <zhaolingyun@zhao.local>
2015-08-17 08:54:40 +0800

    Initial Commit

---
+++ day02/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  day02
+//
+//  Created by 赵凌云 on 15/8/17.
+//  Copyright (c) 2015年 赵凌云. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
